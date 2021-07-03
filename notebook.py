262ee3361d40bd325154d1c28bc89c729ffba0fe
julien loiseau <julien.loiseau@edu.escp.eu>
2021-07-03 15:05:59 +0200

    CSV file data retrieval & check

--- notebook.py
+++ notebook.py
@@ -1,1 +1,7 @@
 import pandas as pd
+# import matplotlib.pyplot as plt
+
+data_url_1 = "https://gist.githubusercontent.com/mbonsma/8da0990b71ba9a09f7de395574e54df1/raw/aec88b30af87fad8d45da7e774223f91dad09e88/lh_data.csv"
+lefthanded_data = pd.read_csv(data_url_1)
+
+print(lefthanded_data.head(5))
